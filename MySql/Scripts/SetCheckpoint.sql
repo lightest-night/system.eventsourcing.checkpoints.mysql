a07cb59c63e938789f07174da395cc0d7f0aa687
James Woodley <james@lightestnight.com>
2020-09-04 10:03:40 +0100

    Use SHA1 and UNHEX to check for duplicate checkpoints

--- MySql/Scripts/SetCheckpoint.sql
+++ MySql/Scripts/SetCheckpoint.sql
@@ -1,3 +1,3 @@
 ﻿INSERT INTO __table-name__ (checkpoint_name, checkpoint_name_hash, checkpoint)
-VALUES (@CheckpointName, UNHEX(HEX(LOWER(@CheckpointName))), @Checkpoint)
+VALUES (@CheckpointName, UNHEX(SHA1(@CheckpointName)), @Checkpoint)
 ON DUPLICATE KEY UPDATE checkpoint = @Checkpoint;
